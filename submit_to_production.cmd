4e5b8159086a44b2a9f37d79857cc16d2dcb0152
Larry Rix <ljr1981@msn.com>
2016-07-15 10:53:45 -0400

    Feature added (FTP): Now creates new EXE and pushes to server
    * There are two scripts involved: 1) submit_to_production, which does
    the ec.exe and final_freezing of the EXE, then a copy-out to the root,
    and then a run of the ISS INNO Setup to compile the installer. 2) There
    is a hidden script that then pushes the new installer to the server by
    way of WinSCP FTP.

--- submit_to_production.cmd
+++ submit_to_production.cmd
@@ -1,2 +1,7 @@
+"C:\Program Files (x86)\Eiffel Software\EiffelStudio 16.05 GPL\studio\spec\windows\bin\ec.exe" -finalize -config moonshot.ecf -target moonshot
+cd EIFGENs\moonshot\F_code
+"C:\Program Files (x86)\Eiffel Software\EiffelStudio 16.05 GPL\studio\spec\windows\bin\finish_freezing.exe"
+cd ..\..\..
 copy /Y .\EIFGENs\moonshot\F_code\moonshot.exe
 "C:\Program Files (x86)\Inno Setup 5\iscc.exe" "moonshot.iss"
+"C:\Program Files (x86)\WinSCP\WinSCP.exe" /script=ftp_to_production.txt
